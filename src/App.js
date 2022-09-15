5c618920a5f86c6661b7525b89941fadb16d6ce1
fuilin <ngfuilin@hotmail.com>
2022-09-15 12:14:44 +0300

    add PageNotFound component

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import Home from "./routes/Home.js"
 import Detail from "./routes/Detail.js"
 import Layout from "./Layout.js"
 import React from 'react';
+import PageNotFound from './PageNotFound';
 
 function App() {
 
-   
+  
+
   return (
     <>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />    
           <Route path="detail/:id" element={<Detail />} />
-          <Route path="*" element={<p>Not found!</p>} />
+          <Route path="*" element={<PageNotFound />} />
         </Route>
       </Routes>
     </> 
